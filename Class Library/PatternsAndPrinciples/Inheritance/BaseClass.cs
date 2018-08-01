dd4129b0be573151c67a68d928542f34f694cbc5
Carl <carlp@grindrodbank.co.za>
2018-08-01 15:10:03 +0200

    Inheritance examples

---
+++ Class Library/PatternsAndPrinciples/Inheritance/BaseClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+//https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/inheritance
+namespace Inheritance
+{
+    public class BaseClass
+    {
+        //Derived classes can also override inherited members by providing an alternate implementation.
+        public virtual void Method1()
+        {
+            Console.WriteLine("DerivedClass behavior");
+        }
+    }
+
+    public abstract class BaseClassAbstract
+    {
+        //Base class members marked with the abstract keyword require that derived classes override them.
+        public abstract void Method1();
+    }
+}
---
+++ Class Library/PatternsAndPrinciples/Inheritance/DerivedClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Inheritance
+{
+    public class DerivedClass : BaseClass
+    {
+        public override void Method1()
+        {
+            Console.WriteLine("DerivedClass - override Method1");
+        }
+    }
+
+    public class DerivedClassAbstract : BaseClassAbstract
+    {
+        public override void Method1()
+        {
+            Console.WriteLine("DerivedClassAbstract - override Method1");
+        }
+    }
+}
