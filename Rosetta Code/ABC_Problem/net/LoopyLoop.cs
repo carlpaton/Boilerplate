1d61edbc344150556d4fafbc07dbda65efd7848c
Carl <carl.paton@gmail.com>
2020-07-01 15:06:36 +1200

    ABC_Problem c# using loop

---
+++ Rosetta Code/ABC_Problem/net/LoopyLoop.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ABC_Problem
+{
+    public class LoopyLoop
+    {
+        public bool MakeWord(string word, List<string> blocks) 
+        {
+            var blocksLeft = blocks;
+            var stringComparison = StringComparison.CurrentCultureIgnoreCase;
+
+            foreach (var letter in word)
+            {
+                if (!blocksLeft.Any(b => b.Contains(letter, stringComparison)))
+                    return false;
+
+                blocksLeft.Remove(blocksLeft.First(b => b.Contains(letter, stringComparison)));
+            }
+
+            return true;
+        }
+    }
+}
