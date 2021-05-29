b7c8a94fbd284764b7bf833b6eed5a7681f5530f
Carl <carl.paton@gmail.com>
2021-05-30 04:43:53 +1200

    Equilibrium Index

---
+++ Rosetta Code/Equilibrium Index/EquilibriumIndex/EquilibriumIndexService.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace EquilibriumIndex
+{
+    public class EquilibriumIndexService
+    {
+        private readonly int[] _array;
+
+        public EquilibriumIndexService(int[] array)
+        {
+            _array = array;
+        }
+
+        public int ForLoop() 
+        {
+            for (int i = 0; i < _array.Length; i++)
+            {
+                if (i == 0 || i == _array.Length - 1)
+                    continue;
+
+                var lowerValues = 0;
+                var upperValues = 0;
+
+                for (int ii = 0; ii < i; ii++)
+                {
+                    lowerValues += _array[ii];
+                }
+
+                for (int iii = _array.Length-1; iii > i; iii--)
+                {
+                    upperValues += _array[iii];
+                }
+
+                if (lowerValues == upperValues)
+                    return i;
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// http://rosettacode.org/wiki/Equilibrium_index#C.23
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<int> ForeachLoop()
+        {
+            var left = 0;
+            var right = _array.Sum();
+            var index = 0;
+            foreach (var element in _array)
+            {
+                right -= element;
+                if (left == right)
+                {
+                    yield return index;
+                }
+                left += element;
+                index++;
+            }
+        }
+    }
+}
---
+++ Rosetta Code/Equilibrium Index/EquilibriumIndex/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EquilibriumIndex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>
+            {
+                2,
+                2,
+                4,
+                1,
+                3
+            };
+
+            list = new List<int>
+            {
+                1,
+                2,
+                3,
+                4,
+                5
+            };
+
+            var equilibriumIndexService = new EquilibriumIndexService(list.ToArray());
+            var result = equilibriumIndexService.ForLoop();
+
+            Console.WriteLine($"Expect 2, Got {result}");
+            Console.WriteLine("-------");
+
+
+            var result2 = equilibriumIndexService
+                .ForeachLoop()
+                .FirstOrDefault();
+
+            Console.WriteLine($"Expect 2, Got {result2}");
+            Console.WriteLine("-------");
+        }
+    }
+}
